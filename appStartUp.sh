780e734eeac40b57e091197e4601b07c8e5fa88a
Kiril Kartunov <kiril.kartunov@gmail.com>
2025-02-24 09:34:33 +0200

    update appStartUp

--- appStartUp.sh
+++ appStartUp.sh
@@ -1,37 +1,16 @@
 #!/bin/bash
 set -eo pipefail
 
-# Environments are created on the fly in dev and so the DB url must be constructed.
-# The CI/CD tool passes the database name to the CloudFormation template which includes it as a env var
-# Here we replace the "DATABASE_NAME" placeholder in the dev connection string with that
-export DATABASE_URL=$(echo -e ${DATABASE_URL/DATABASE_NAME/$DATABASE_NAME})
+export DATABASE_URL=$(echo -e ${DATABASE_URL})
 
-# Query for the database name for this deployment
-dbnameselect=$(psql -P t -P format=unaligned "${DATABASE_ADMIN_URL}" -c "SELECT datname FROM pg_database where datname = '${DATABASE_NAME}';")
-
-# If the database exists run migrations or reset
-if [[ "$dbnameselect" == "$DATABASE_NAME" ]]; then
-    echo "Database ${DATABASE_NAME} exists - running migrations."
-    if $RESET_DB; then
-        echo "Resetting DB"
-        npx prisma migrate reset --force
-        echo "Seeding database"
-        npm run seed -- $SEED_DATA
-    else
-        echo "Running migrations"
-        npx prisma migrate deploy
-    fi
-else
-    # Else create the DB and reset
-    echo "Database ${DATABASE_NAME} does not exist - creating."
-    psql -P t -P format=unaligned "${DATABASE_ADMIN_URL}" -c "CREATE DATABASE \"${DATABASE_NAME}\" OWNER ${DATABASE_ADMIN_USER};"
-
-    echo "Running migrations."
+echo "Database - running migrations."
+if $RESET_DB; then
+    echo "Resetting DB"
     npx prisma migrate reset --force
-
-    echo "Seeding database"
-    npm run seed -- $SEED_DATA
+else
+    echo "Running migrations"
+    npx prisma migrate deploy
 fi
 
 # Start the app
-npm start
+pnpm start
