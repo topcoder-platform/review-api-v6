b45d597b8529a9c25cb00b18d19c9a48e260bf34
Gunasekar-K <gunasekar.kesavalu@wipro.com>
2025-02-24 13:15:14 +0530

    CI-Integration

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -eo pipefail
-docker build --no-cache=true --build-arg RESET_DB_ARG=<<pipeline.parameters.reset-db>> --build-arg SEED_DATA_ARG=${DEPLOYMENT_ENVIRONMENT} -t ${APPNAME}}:latest .
+docker buildx build --no-cache=true --build-arg RESET_DB_ARG=<<pipeline.parameters.reset-db>> --build-arg SEED_DATA_ARG=${DEPLOYMENT_ENVIRONMENT} -t ${APPNAME}}:latest .
