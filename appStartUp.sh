c9060c527a3fdafa9a4d24454e9c50e23fa7c350
Kiril Kartunov <kiril.kartunov@gmail.com>
2025-02-25 10:15:24 +0200

    correct fix appStartUp

--- appStartUp.sh
+++ appStartUp.sh
@@ -6,11 +6,11 @@ export DATABASE_URL=$(echo -e ${DATABASE_URL})
 echo "Database - running migrations."
 if $RESET_DB; then
     echo "Resetting DB"
-    RUN npx prisma -v
+    npx prisma -v
     npx prisma migrate reset --force
 else
     echo "Running migrations"
-    RUN npx prisma -v
+    npx prisma -v
     npx prisma migrate deploy
 fi
 
