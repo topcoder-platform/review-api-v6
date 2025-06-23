aae1df9eb5a066f18ea15769b8a9928d8631d13f
billsedison <billsedison@163.com>
2025-06-23 11:05:59 +0800

    Add IsString and IsNotEmpty decorators back

--- src/dto/reviewOpportunity.dto.ts
+++ src/dto/reviewOpportunity.dto.ts
@@ -46,6 +46,8 @@ export class CreateReviewOpportunityDto {
   @ApiProperty({
     description: 'Challenge id',
   })
+  @IsString()
+  @IsNotEmpty()
   @IsUUID()
   challengeId: string;
 
@@ -141,6 +143,8 @@ export class ReviewOpportunityResponseDto extends CreateReviewOpportunityDto {
   @ApiProperty({
     description: 'Review opportunity id',
   })
+  @IsString()
+  @IsNotEmpty()
   @IsUUID()
   id: string;
 
