262b32dfd9bfd2ba6c095ad53d2ae55ab733426d
himaniraghav3 <himaniraghav0311@gmail.com>
2025-09-17 16:14:58 +0530

    fix: application date for review opporunity

--- src/api/review-opportunity/reviewOpportunity.service.ts
+++ src/api/review-opportunity/reviewOpportunity.service.ts
@@ -294,7 +294,7 @@ export class ReviewOpportunityService {
         handle: e.handle,
         role: convertRoleName(e.role),
         status: e.status,
-        applicationDate: e.createdBy,
+        applicationDate: e.createdAt,
       }));
     }
 
