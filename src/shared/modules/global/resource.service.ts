ba7a2015670e58b87c520021c9bb9207cb996b2f
Kiril Kartunov <kiril.kartunov@gmail.com>
2025-11-02 13:06:21 +0200

    add debug logs

--- src/shared/modules/global/resource.service.ts
+++ src/shared/modules/global/resource.service.ts
@@ -97,6 +97,12 @@ export class ResourceApiService {
       while (true) {
         params.set('page', String(page));
 
+        this.logger.log(
+          'Debug resources call:',
+          params.toString(),
+          JSON.stringify(params),
+        );
+
         const url = `${CommonConfig.apis.resourceApiUrl}resources?${params.toString()}`;
         const response = await firstValueFrom(
           this.httpService.get<ResourceInfo[]>(url, {
