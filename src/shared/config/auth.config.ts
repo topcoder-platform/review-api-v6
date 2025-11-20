d6c9749624fe5047875d2be7a4712e32bae34d78
Justin Gasper <jmgasper@gmail.com>
2025-11-20 12:15:08 +1100

    Fixes for local issues seen with JWT validation, in case it starts to affect dev and prod

--- src/shared/config/auth.config.ts
+++ src/shared/config/auth.config.ts
@@ -8,7 +8,7 @@ export const AuthConfig = {
   // JSON array string of allowed issuers
   validIssuers:
     process.env.VALID_ISSUERS ||
-    '["https://testsachin.topcoder-dev.com/","https://test-sachin-rs256.auth0.com/","https://api.topcoder.com","https://api.topcoder-dev.com","https://topcoder-dev.auth0.com/", "https://auth.topcoder-dev.com/"]',
+    '["https://testsachin.topcoder-dev.com/","https://test-sachin-rs256.auth0.com/","https://api.topcoder.com","https://api.topcoder-dev.com","https://topcoder-dev.auth0.com/","https://auth.topcoder-dev.com/","https://topcoder.auth0.com/","https://auth.topcoder.com/"]',
 
   // Legacy JWT configuration (kept for backward compatibility)
   jwt: {
