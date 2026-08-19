d90b3d4a220fd9e3586305e0781b30a24e1d3a25
Vasilica Olariu <olariu.vasilica@gmail.com>
2026-08-19 08:00:30 +0300

    PM-5923 - MyReview - make sure only "placement" type challenge winners are considered

--- src/api/my-review/myReview.service.ts
+++ src/api/my-review/myReview.service.ts
@@ -858,6 +858,7 @@ export class MyReviewService {
             ) AS winners
           FROM challenges."ChallengeWinner" w
           WHERE w."challengeId" = bp."challengeId"
+          AND w.type = 'PLACEMENT'
         ) cw ON TRUE
         LEFT JOIN LATERAL (
           SELECT
@@ -969,6 +970,7 @@ export class MyReviewService {
             ) AS winners
           FROM challenges."ChallengeWinner" w
           WHERE w."challengeId" = bp."challengeId"
+          AND w.type = 'PLACEMENT'
         ) cw ON TRUE
         LEFT JOIN LATERAL (
           SELECT
